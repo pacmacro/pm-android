19abc60fad84206b207d8a32c73e8d881be60c5e
HarryStarfrstCui <cuihy1999@gmail.com>
2019-06-13 15:26:54 -0700

    refactored lobby activity&asset names for Subin: idk if you'll work on anything other than the credit, but if you do, just know that i plan to make the first 3 screen as fragments.

--- app/src/main/java/ca/sfu/pacmacro/LobbyActivity.java
+++ app/src/main/java/ca/sfu/pacmacro/LobbyActivity.java
@@ -2,108 +2,33 @@ package ca.sfu.pacmacro;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
+import ca.sfu.pacmacro.Model.HomeFragment;
+
 public class LobbyActivity extends AppCompatActivity {
-
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
-    private SectionsPagerAdapter mSectionsPagerAdapter;
-
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
-    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lobby);
-
-        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
-
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.container);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
-
-        //TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
-        //tabLayout.setupWithViewPager(mViewPager);
-
+        if(savedInstanceState == null) {
+            changeFrame(new HomeFragment());
+        }
     }
 
-
-    /*@Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_lobby, menu);
-        return true;
+    public void changeFrame(Fragment nextFrame){
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, nextFrame)
+                .addToBackStack(null).commit();
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_credits) {
-            Intent intent = new Intent(this, CreditsActivity.class);
-            startActivity(intent);
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }*/
-
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-            switch (position) {
-                case 0:
-                    return SpectatorFragment.newInstance();
-                case 1:
-                    return PlayerFragment.newInstance();
-            }
-            return null;
-        }
-
-        @Override
-        public int getCount() {
-            return 2;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0:
-                    return "Spectator";
-                case 1:
-                    return "Player";
-            }
-            return null;
+    public void onBackPressed() {
+        int count = getSupportFragmentManager().getBackStackEntryCount();
+        if (count == 0) {
+            super.onBackPressed();
+        } else {
+            getSupportFragmentManager().popBackStack();
         }
     }
 }
--- app/src/main/java/ca/sfu/pacmacro/Model/HomeFragment.java
+++ app/src/main/java/ca/sfu/pacmacro/Model/HomeFragment.java
@@ -1,7 +1,29 @@
 package ca.sfu.pacmacro.Model;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import ca.sfu.pacmacro.R;
 
 public class HomeFragment extends Fragment {
 
+    private ImageView mPlayer;
+    private ImageView mSpectator;
+    private ImageView mCredit;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_lobby, container, false);
+        mPlayer = view.findViewById(R.id.homePlayer);
+        mSpectator = view.findViewById(R.id.homeSpectator);
+        mCredit = view.findViewById(R.id.homeCredit);
+
+        return view;
+    }
 }
